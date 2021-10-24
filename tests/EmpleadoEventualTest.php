521d7d402737849128a3ace4f97135924b1a4cbb
Geronimo <geroalde@live.com.ar>
2021-10-24 18:08:03 -0300

    Ultimo commit. Limpieza de errores y finalizacion del trabajo

--- tests/EmpleadoEventualTest.php
+++ tests/EmpleadoEventualTest.php
@@ -10,5 +10,16 @@ class EmpleadoEventualTest extends EmpleadoTest {
         $t = new \App\EmpleadoEventual($nombre, $apellido, $dni, $salario, $monto);
         return $t;
     }
-    
+ public function testSeCalculaLaComision() {
+    $t= $this->create("Geronimo", "Alderette", 42767236, 2000);
+    $this->assertEquals(12.5, $t->calcularComision());
+ }
+ public function testSeCalculaElIngresoTotal() {
+    $this->expectException(\Exception::class);
+    $t= $this->create("Geronimo", "Alderette", 42767236, 2000, null, [0,-100, 200, 300]);
+ }
+ public function testSeIngresoUnMontoDeVentaNegativoOCero() {
+    $t= $this->create("Geronimo" , "Alderette", 42767236, 2000);
+    $this->assertEquals(2012.5, $t->calcularIngresoTotal());
+ }
 }
