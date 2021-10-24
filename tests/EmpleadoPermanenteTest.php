cd2dcfb0c617aad9c0682bb947ba0cd2a45e6f0d
Geronimo <geroalde@live.com.ar>
2021-10-23 21:14:58 -0300

    Terminacion de EmpleadoPermanenteTest.php y comprobacion de funcionalidad

--- tests/EmpleadoPermanenteTest.php
+++ tests/EmpleadoPermanenteTest.php
@@ -10,5 +10,45 @@ class EmpleadoPermanenteTest extends EmpleadoTest {
         $t = new \App\EmpleadoPermanente($nombre, $apellido, $dni, $salario, $fecha);
         return $t;
      }
-     
+     public function testIngresarFechaIngreso() {
+        $f = new DateTime('2000-07-06');
+        $t = $this->create("Geronimo", "Alderette", 42767236, 2000, $f);
+        $this->assertEquals('2000-07-06', $t->getfechaIngreso()->format("Y-m-d"));
+      }
+      public function testCalcularLaComisionDeterminada() {
+        $fecha = new DateTime('2000-07-06');
+        $t = $this->create("Geronimo", "Alderette", 42767236, 1000, $fecha);
+        $fecha2 = new DateTime();
+        $antiguedad = $fecha2->diff($fecha);
+        $this->assertEquals($antiguedad->y, $t->calcularAntiguedad());
+        $this->assertEquals("{$antiguedad->y}%", $t->calcularComision());
+      }
+      public function testCalcularElIngresoTotalCorrectamente() {
+        $fecha = new DateTime('2000-07-06');
+        $antiguedad = 5;
+        $t = $this->create("Geronimo", "Alderette", 42767236, 1000, $fecha, $antiguedad);
+        $this->assertEquals(1210, $t->calcularIngresoTotal());
+      }
+      public function testComprobarElFuncionamientoBasadoEnLaAntiguedad() {
+        $fecha = new DateTime('2000-07-06');
+        $t = $this->create("Geronimo", "Alderette", 42767236, 1000, $fecha);
+        $fecha2 = new DateTime();
+        $antiguedad = $fecha2->diff($fecha);
+        $this->assertEquals($antiguedad->y, $t->calcularAntiguedad());
+      }
+      public function testComprobarSituacionAlNoProporcionarFechaDeIngreso() {
+        $fecha = new DateTime();
+        $t = $this->create("Geronimo", "Alderette", 42767236, 1000, $fecha);
+        $fecha2 = new DateTime();
+        $antiguedad = $fecha->diff($fecha2);
+        $this->assertEquals($antiguedad->y, $t->calcularAntiguedad());
+        $this->assertEquals("0%", $t->calcularComision());
+      }
+      public function testComprobarFechaDeIngresoPosteriorALaActual() {
+        $this->expectException(\Exception::class);
+        $fecha = new DateTime('2022-12-12');
+        $t = $this->create("Geronimo", "Alderette", 42767236, 1000, $fecha);
+        $fecha2 = new DateTime();
+        $antiguedad = $fecha2->diff($fecha);
+      }
 }
